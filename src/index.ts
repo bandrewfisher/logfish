65b128b52d9f9aa853de9f6496d2261d26f3d038
Brett Fisher <brett.fisher@endurance.com>
2020-04-12 15:46:57 -0600

    Add log level methods and add tests

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-import express from 'express';
+import axios from 'axios';
 
-const app = express();
-app.get('/', (_req, res) => {
-  res.send('Hello world!');
-});
+export type LogConfigLevel = 'INFO' | 'WARN' | 'ERROR' | 'DEBUG';
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+export interface LogConfig {
+  level: LogConfigLevel;
+}
+export default class Logfish {
+  private axiosInstance: import('axios').AxiosInstance;
+
+  constructor(apiKey: string, url: string) {
+    this.axiosInstance = axios.create({
+      baseURL: url,
+    });
+    this.axiosInstance.defaults.headers['logfish-key'] = apiKey;
+  }
+
+  public log(data: any, { level }: LogConfig = { level: 'INFO' }) {
+    this.axiosInstance.post('/log', `${level}: ${data}`);
+  }
+
+  public info(data: any) {
+    this.log(data, { level: 'INFO' });
+  }
+
+  public warn(data: any) {
+    this.log(data, { level: 'WARN' });
+  }
+
+  public debug(data: any) {
+    this.log(data, { level: 'DEBUG' });
+  }
+
+  public error(data: any) {
+    this.log(data, { level: 'ERROR' });
+  }
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,83 @@
-describe('example', () => {
-  it('runs tests', () => {
-    expect(true).toBeTruthy();
+import axios from 'axios';
+import Logfish, { LogConfigLevel } from '../src/index'; // eslint-disable-line no-unused-vars
+
+const mockPost = jest.fn();
+
+interface MockAxiosDefaults {
+  headers: {
+    'logfish-key'?: string
+  }
+}
+const mockAxiosDefaults: MockAxiosDefaults = {
+  headers: {},
+};
+jest.mock('axios', () => ({
+  create: jest.fn(() => ({
+    post: mockPost,
+    defaults: mockAxiosDefaults,
+  })),
+}));
+
+const POST_ENDPOINT = '/log';
+const MESSAGE = 'message';
+const MOCK_KEY = 'key';
+const MOCK_URL = 'url';
+
+const logMatch = (level: LogConfigLevel, message: any = MESSAGE) => {
+  const lastPostCall = mockPost.mock.calls[mockPost.mock.calls.length - 1];
+  return lastPostCall[0] === POST_ENDPOINT && lastPostCall[1] === `${level}: ${message}`;
+};
+
+const createLogfish = (): Logfish => new Logfish(MOCK_KEY, MOCK_URL);
+
+test('creates axios object', () => {
+  createLogfish();
+  expect(axios.create).toHaveBeenCalledWith({ baseURL: MOCK_URL });
+  expect(mockAxiosDefaults.headers['logfish-key']).toBe(MOCK_KEY);
+});
+
+test('posts data to url', () => {
+  const logfish = createLogfish();
+  logfish.log(MESSAGE);
+  expect(mockPost).toHaveBeenCalled();
+});
+
+test('INFO is default level', () => {
+  const logfish = createLogfish();
+  logfish.log(MESSAGE);
+  expect(logMatch('INFO')).toBe(true);
+});
+
+test('custom levels are used', () => {
+  const logfish = createLogfish();
+  const levels: LogConfigLevel[] = ['DEBUG', 'INFO', 'WARN', 'ERROR'];
+
+  levels.forEach((level) => {
+    logfish.log(MESSAGE, { level });
+    expect(logMatch(level)).toBe(true);
   });
+});
+
+test('info method logs with INFO', () => {
+  const logfish = createLogfish();
+  logfish.info(MESSAGE);
+  expect(logMatch('INFO')).toBe(true);
+});
+
+test('warn method logs with WARN', () => {
+  const logfish = createLogfish();
+  logfish.warn(MESSAGE);
+  expect(logMatch('WARN')).toBe(true);
+});
+
+test('debug method logs with DEBUG', () => {
+  const logfish = createLogfish();
+  logfish.debug(MESSAGE);
+  expect(logMatch('DEBUG')).toBe(true);
+});
+
+test('error method logs with ERROR', () => {
+  const logfish = createLogfish();
+  logfish.error(MESSAGE);
+  expect(logMatch('ERROR')).toBe(true);
 });
